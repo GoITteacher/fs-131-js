e872229195b819e5c8272bee8a64ef121da7bc69
Volodymyr Pashchenko <pikimelposhta@gmail.com>
2026-09-25 21:27:33 +0300

    update

--- js/01-class.js
+++ js/01-class.js
@@ -8,3 +8,102 @@
  *  - Геттери і сеттери
  *  - Статичні властивості
  */
+
+//!=========================================
+
+// class Car {
+//   constructor(model, price, year) {
+//     this.brand = 'Tesla';
+//     this.discount = 5;
+
+//     this.model = model;
+//     this.price = price;
+//     this.year = year;
+//   }
+
+//   showShortInfo() {
+//     console.log(`${this.brand} ${this.model}`);
+//   }
+
+//   showFullInfo() {
+//     console.log(`Brand: ${this.brand}`);
+//     console.log(`Model: ${this.model}`);
+//     console.log(`Price: ${this.price}`);
+//     console.log(`Year: ${this.year}`);
+//   }
+// }
+
+// const car1 = new Car('S', 500, 2022);
+// const car2 = new Car('E', 600, 2023);
+// const car3 = new Car();
+
+// console.log(car3);
+
+//!=========================================
+
+// class User {
+//   #age;
+//   #balance;
+//   #discount = 10;
+//   static amount = 0;
+
+//   constructor(name, age, balance) {
+//     User.amount += 1;
+//     this.name = name;
+//     this.#age = age;
+//     this.#balance = balance;
+//   }
+
+//   static showInstruction() {
+//     console.log('Це клас для створення користувачів');
+//     console.log('Конструктору потрібно передавати імя вік і баланс');
+//   }
+
+//   #showName() {
+//     console.log(`Name: ${this.name}`);
+//   }
+
+//   #showAge() {
+//     console.log(`Age: ${this.#age}`);
+//   }
+
+//   #showBalance() {
+//     console.log(`Balance: ${this.#balance}`);
+//   }
+
+//   showInfo() {
+//     this.#showName();
+//     this.#showBalance();
+//     this.#showAge();
+//   }
+
+//   get balance() {
+//     return `Balance: ${this.#balance}`;
+//   }
+
+//   set balance(newValue) {
+//     if (newValue >= 0 && newValue <= 100000) {
+//       this.#balance = newValue;
+//     }
+//   }
+
+//   get discount() {
+//     return this.#discount;
+//   }
+// }
+
+// const user1 = new User('Vasya', 25, 200);
+// const user2 = new User('Vasya', 25, 200);
+// const user3 = new User('Vasya', 25, 200);
+// const user4 = new User('Vasya', 25, 200);
+
+// console.log(User.amount);
+
+// console.log(user1.balance);
+
+// user1.balance = 50;
+
+// user1.discount = 25;
+// console.log(user1.discount);
+
+//!=========================================
--- js/02-inheritance.js
+++ js/02-inheritance.js
@@ -5,3 +5,36 @@
  *  - Конструктор дочірнього класу (super)
  *  - Методи дочірнього класу
  */
+
+class Animal {
+  constructor(breed, name, age) {
+    this.breed = breed;
+    this.name = name;
+    this.age = age;
+  }
+
+  eat() {
+    console.log('Ням ням');
+  }
+  sleep() {
+    console.log('Zzzzzz');
+  }
+  showInfo() {
+    console.log(this.name);
+    console.log(this.breed);
+    console.log(this.age);
+  }
+}
+
+class Dog extends Animal {
+  constructor(breed, name, age) {
+    super(breed, name, age);
+    this.paws = 4;
+  }
+
+  sayWoof() {}
+}
+
+const dog1 = new Dog('spaniel', 'Lord', 5);
+
+console.log(dog1);
